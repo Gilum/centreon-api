2111c885cce62f55dab3d520d46511cf0bde2715
root <beulque.yann@gmail.com>
2017-04-17 16:47:41 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cp $DIR/conf/11-centreon-api.conf /etc/httpd/conf.d/
-service httpd reload
+#service httpd reload
 
 rsync -avH --delete $DIR/www/modules/centreon-api/ /usr/share/centreon/www/modules/centreon-api/
