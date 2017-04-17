648d6f0fe3ccd4c5b43b5043c1cb85d97b96865e
root <beulque.yann@gmail.com>
2017-04-17 16:40:27 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-cp $PWD/conf/11-centreon-api.conf /etc/httpd/conf.d/
-service httpd reload
+echo "Install OK"
+#cp $PWD/conf/11-centreon-api.conf /etc/httpd/conf.d/
+#service httpd reload
 
-rsync -avH --delete $PWD/www/modules/centreon-api/ /usr/share/centreon/www/modules/centreon-api/
+#rsync -avH --delete $PWD/www/modules/centreon-api/ /usr/share/centreon/www/modules/centreon-api/
